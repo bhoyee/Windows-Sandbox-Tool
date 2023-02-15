fef7f33c6e50fff4c92fcd4ecd086564a12a5d17
bhoyee <s.adeboye@outlook.com>
2023-02-15 20:41:06 +0100

    added initial files

---
+++ sandbox/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.programTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.argumentsTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // programTextBox
+            // 
+            this.programTextBox.Location = new System.Drawing.Point(33, 61);
+            this.programTextBox.Name = "programTextBox";
+            this.programTextBox.Size = new System.Drawing.Size(346, 20);
+            this.programTextBox.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "select file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(33, 118);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+           // this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(546, 104);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(208, 96);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Text = "";
+            // 
+            // argumentsTextBox
+            // 
+            this.argumentsTextBox.Location = new System.Drawing.Point(171, 174);
+            this.argumentsTextBox.Name = "argumentsTextBox";
+            this.argumentsTextBox.Size = new System.Drawing.Size(302, 20);
+            this.argumentsTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.argumentsTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.programTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox programTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRun;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.TextBox argumentsTextBox;
+    }
+}
+
---
+++ sandbox/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+
+namespace sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                programTextBox.Text = openFileDialog1.FileName;
+            }
+            
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            var path = programTextBox.Text;
+            var arguments = argumentsTextBox.Text;
+
+            // Create a new AppDomain for the application to run in
+            AppDomain sandbox = AppDomain.CreateDomain("Sandbox", null, new AppDomainSetup
+            {
+                ApplicationBase = Path.GetDirectoryName(path),
+                ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile,
+                ApplicationName = Path.GetFileNameWithoutExtension(path)
+            });
+
+            try
+            {
+                // Load the application into the sandbox
+                Assembly assembly = sandbox.Load(AssemblyName.GetAssemblyName(path));
+
+                // Get a reference to the Main method of the application
+                MethodInfo method = assembly.EntryPoint;
+                if (method != null)
+                {
+                    // Execute the Main method in the sandbox
+                    method.Invoke(null, new object[] { arguments.Split(' ') });
+                }
+            }
+            finally
+            {
+                // Unload the sandbox
+                AppDomain.Unload(sandbox);
+            }
+        }
+    }
+
+}
